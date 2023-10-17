2450366a2ba1ec8d87b49472fb48b4a1555015fe
Aryan Jaiswal <aryanj868@gmai.com>
2023-10-17 21:11:47 +0530

    first commit

--- src/store/app-context.js
+++
@@ -1,9 +0,0 @@
-const AppContext = React.createContext({
-    items: [],
-    totalAmount: 0,
-    addItem: (item) => {},
-    removeItem: (id) => {},
-    clearCart: () => {}
-});
-
-export default AppContext;
